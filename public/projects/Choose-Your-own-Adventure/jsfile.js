47b71fd18219cb5b9cfa4223d4c6c6ac2cbb28a8
Devi Damsel <scstolp@gmail.com>
2019-03-08 23:56:15 -0700

    New projects

---
+++ public/projects/Choose-Your-own-Adventure/jsfile.js
@@ -0,0 +1,9 @@
+function myFunction() {
+   alert("clicked!");
+}
+
+function changeColor() {
+    let color = document.getElementById("color").value;
+    document.getElementById("div1").style.color = color;
+
+}
