e826a5822496d31e9a0d61c9d7b3f8b95589b889
Devi Damsel <scstolp@gmail.com>
2019-03-16 15:47:42 -0600

    making changes

--- public/projects/Choose-Your-own-Adventure/jsfile.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-   alert("clicked!");
-}
-
-function changeColor() {
-    let color = document.getElementById("color").value;
-    document.getElementById("div1").style.color = color;
-
-}
