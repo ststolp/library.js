cb75223f3c2d5fba63e5382cd156b8964aab1340
Devi Damsel <scstolp@gmail.com>
2019-03-16 19:13:19 -0600

    adding database

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.set("view engine", "ejs");
 
 app.get("/", redirectUser);
 app.get("/get_library", getLibrary);
-//app.get("/search_library", searchLibrary);
+app.get("/search_library", searchLibrary);
 
 app.listen(app.get('port'), function(){
 	console.log("It's working");
@@ -48,40 +48,40 @@ function getAllBooks(callback_lib) {
 }
  
 
-// function searchLibrary(req, response) {
-//     const method = req.query.method;
-//     const search = req.query.search;
+function searchLibrary(req, response) {
+    const method = req.query.method;
+    const search = req.query.search;
 
-//     getBooks(method, search, function(error, result) {
-//               if (error || result == null || result.length != 1) {
-// 			response.status(500).json({success: false, data: error});
-// 		} else {
-// 			const books = result;
-// 			response.status(200).json(books);
-// 		}
-//     });
-// }
+    getBooks(method, search, function(error, result) {
+              if (error || result == null) {
+			response.status(500).json({success: false, data: error});
+		} else {
+			const books = result;
+			response.status(200).json(books);
+		}
+    });
+}
 
-// function getBooks(method, search, callback) {
-//     let query = "";
-//     if (search != "") {
-// 	   if (method == 'lname') {
-// 		query = "SELECT b.title, a.fname, a.lname, b.year, b.publisher FROM books b INNER JOIN author a ON b.author_id = a.author_id WHERE a.lname = $1 ORDER BY b.title";
-//        } else if (method == 'title') {
-// 		query = "SELECT b.title, a.fname, a.lname, b.year, b.publisher FROM books b INNER JOIN author a ON b.author_id = a.author_id WHERE b.title = $1 ORDER BY b.title";	
-// 	   } else if (method == 'genre') {
-//        	query = "SELECT b.title, a.fname, a.lname, b.year, b.publisher FROM books b INNER JOIN author a ON b.author_id = a.author_id INNER JOIN genre g ON a.genre_id = g.genre_id WHERE g.genre = $1 ORDER BY b.title";
-//        } else {
-// 	     query = "SELECT b.title, a.fname, a.lname, b.year, b.publisher FROM books b INNER JOIN author a ON b.author_id = a.author_id WHERE b.title = $1 ORDER BY b.title";
-//        }
-//     }
-//     const params = [search];
-//     console.log("The search is : " + search);
-//     pool.query(query, params, function(error, response) {
-//         if (error) {
-//             console.log("There was an error" + error);
-//             callback(error, null);
-//         }
-//         callback(null, response.rows);
-//     });
-// }
+function getBooks(method, search, callback) {
+    let query = "";
+    if (search != "") {
+	   if (method == 'lname') {
+		query = "SELECT b.title, a.fname, a.lname, b.year, b.publisher FROM books b INNER JOIN author a ON b.author_id = a.author_id WHERE a.lname = $1 ORDER BY b.title";
+       } else if (method == 'title') {
+		query = "SELECT b.title, a.fname, a.lname, b.year, b.publisher FROM books b INNER JOIN author a ON b.author_id = a.author_id WHERE b.title = $1 ORDER BY b.title";	
+	   } else if (method == 'genre') {
+       	query = "SELECT b.title, a.fname, a.lname, b.year, b.publisher FROM books b INNER JOIN author a ON b.author_id = a.author_id INNER JOIN genre g ON a.genre_id = g.genre_id WHERE g.genre = $1 ORDER BY b.title";
+       } else {
+	     query = "SELECT b.title, a.fname, a.lname, b.year, b.publisher FROM books b INNER JOIN author a ON b.author_id = a.author_id WHERE b.title = $1 ORDER BY b.title";
+       }
+    }
+    const params = [search];
+    console.log("The search is : " + search);
+    pool.query(query, params, function(error, response) {
+        if (error) {
+            console.log("There was an error" + error);
+            callback(error, null);
+        }
+        callback(null, response.rows);
+    });
+}
