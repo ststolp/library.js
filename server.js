b7c89123e882b05950b043b2a183e41b55bdec48
Devi Damsel <scstolp@gmail.com>
2019-03-16 18:46:19 -0600

    adding database

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ var app = express();
 const controller = require("./controllers/libraryFunctions.js");
 app.set('port', (process.env.PORT || 5000));
 const { Pool } = require("pg");
-
-
-
 const connectionString = process.env.DATABASE_URL;
 const pool = Pool({connectionString: connectionString});
 
@@ -28,7 +25,6 @@ function redirectUser(req, res) {
 };
 
 function getLibrary(req, res) {
-
     getAllBooks(function(error, result) {
         if (error || result == null || result.length != 1) {
             res.status(500).json({success: false, data: error});
@@ -39,17 +35,18 @@ function getLibrary(req, res) {
     });
 }
  
-function getAllBooks(callback) {
+function getAllBooks(callback_lib) {
     const query = "SELECT b.book_id, b.title, a.fname, a.lname, b.year, b.publisher FROM books b INNER JOIN author a ON b.author_id = a.author_id ORDER BY b.title";
-   pool.query(query, function(error, response) {
-         if (error) {
-             console.log("There was an error" + error);
-             callback(error, null);
-         }
-        callback(null, response.rows);
-   });
+    pool.query(query, function(error, response) {
+        if (error) {
+            console.log("There was an error" + error);
+            callback(error, null);
+        }
+        callback_lib(null, response.rows);
+    });
 }
  
+
 function searchLibrary(req, response) {
     const method = req.query.method;
     const search = req.query.search;
@@ -67,23 +64,23 @@ function searchLibrary(req, response) {
 function getBooks(method, search, callback) {
     let query = "";
     if (search != "") {
-	if (method == 'lname') {
+	   if (method == 'lname') {
 		query = "SELECT b.title, a.fname, a.lname, b.year, b.publisher FROM books b INNER JOIN author a ON b.author_id = a.author_id WHERE a.lname = '$1' ORDER BY b.title";
-	} else if (method == 'title') {
+       } else if (method == 'title') {
 		query = "SELECT b.title, a.fname, a.lname, b.year, b.publisher FROM books b INNER JOIN author a ON b.author_id = a.author_id WHERE b.title = '$1' ORDER BY b.title";	
-	} else if (method == 'genre') {
+	   } else if (method == 'genre') {
        	query = "SELECT b.title, a.fname, a.lname, b.year, b.publisher FROM books b INNER JOIN author a ON b.author_id = a.author_id INNER JOIN genre g ON a.genre_id = g.genre_id WHERE g.genre = '$1' ORDER BY b.title";
-  } else {
-	query = "SELECT b.title, a.fname, a.lname, b.year, b.publisher FROM books b INNER JOIN author a ON b.author_id = a.author_id WHERE b.title = '$1' ORDER BY b.title";
-    	}
-}
-  const params = [search];
-  console.log("The search is : " + search);
-     pool.query(query, params, function(error, response) {
-         if (error) {
-             console.log("There was an error" + error);
-             callback(error, null);
-         }
+       } else {
+	     query = "SELECT b.title, a.fname, a.lname, b.year, b.publisher FROM books b INNER JOIN author a ON b.author_id = a.author_id WHERE b.title = '$1' ORDER BY b.title";
+       }
+    }
+    const params = [search];
+    console.log("The search is : " + search);
+    pool.query(query, params, function(error, response) {
+        if (error) {
+            console.log("There was an error" + error);
+            callback(error, null);
+        }
         callback(null, response.rows);
-     });
+    });
 }
