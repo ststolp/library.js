973c3f336f9776ddca86dfa3181c6624f2823bbd
Devi Damsel <scstolp@gmail.com>
2019-03-20 16:35:28 -0600

    results

--- server.js
+++ server.js
@@ -66,13 +66,13 @@ function getBooks(method, search, callback) {
     let query = "";
     if (search != "") {
 	   if (method == 'lname') {
-		query = "SELECT b.title, a.fname, a.lname, b.year, b.publisher FROM books b INNER JOIN author a ON b.author_id = a.author_id WHERE a.lname LIKE '%' $1 '%' ORDER BY b.title";
+		query = "SELECT b.title, a.fname, a.lname, b.year, b.publisher FROM books b INNER JOIN author a ON b.author_id = a.author_id WHERE a.lname LIKE '% $1 %' ORDER BY b.title";
        } else if (method == 'title') {
-		query = "SELECT b.title, a.fname, a.lname, b.year, b.publisher FROM books b INNER JOIN author a ON b.author_id = a.author_id WHERE b.title LIKE '%' $1 '%' ORDER BY b.title";	
+		query = "SELECT b.title, a.fname, a.lname, b.year, b.publisher FROM books b INNER JOIN author a ON b.author_id = a.author_id WHERE b.title LIKE '% $1 %' ORDER BY b.title";	
 	   } else if (method == 'genre') {
-       	query = "SELECT b.title, a.fname, a.lname, b.year, b.publisher FROM books b INNER JOIN author a ON b.author_id = a.author_id INNER JOIN genre g ON a.genre_id = g.genre_id WHERE g.genre LIKE '%' $1 '%' ORDER BY b.title";
+       	query = "SELECT b.title, a.fname, a.lname, b.year, b.publisher FROM books b INNER JOIN author a ON b.author_id = a.author_id INNER JOIN genre g ON a.genre_id = g.genre_id WHERE g.genre LIKE '% $1 %' ORDER BY b.title";
        } else {
-	     query = "SELECT b.title, a.fname, a.lname, b.year, b.publisher FROM books b INNER JOIN author a ON b.author_id = a.author_id WHERE b.title LIKE '%' $1 '%' ORDER BY b.title";
+	     query = "SELECT b.title, a.fname, a.lname, b.year, b.publisher FROM books b INNER JOIN author a ON b.author_id = a.author_id WHERE b.title = $1 ORDER BY b.title";
        }
     }
     const params = [search];
