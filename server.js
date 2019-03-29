4a6623c4a85e478cd348320e1d07b56486855063
Devi Damsel <scstolp@gmail.com>
2019-03-29 12:11:01 -0600

    books

--- server.js
+++ server.js
@@ -50,20 +50,20 @@ function checkOut(req, response) {
     let array = req.body.checkout;
     let query = "";
     let url = "checkOut=true";
-    array.forEach(function(item, index, array) {
+    for (let index = 0; index < array.length; index++) {
         if (array[index + 1] == array.length) {  
             return response.status(200).redirect(`home_library.html?${url}`);
         } else {
             query = `INSERT INTO patron_book (patron_id, book_title, book_id, due_date, checked_out) VALUES (1, (SELECT title FROM books WHERE book_id = $1), $2, CURRENT_DATE + interval '30' day, CURRENT_DATE)`;
-            let params = [item, item];
+            let params = [array[index], array[index]];
             pool.query(query, params, function(error, res) {
                 if (error) {
                     console.log(`There was an error: ${error}`);
                     response.redirect(`home_library.html?${url}`);
                 } else {
                     console.log(res);
-                    console.log("Item: " + item);
-                    url += "&array[]=" + item;
+                    console.log("Item: " + array[index]);
+                    url += "&array[]=" + array[index];
                     console.log("Index: " + index + " arrayLength: " + array.length);
                 }
             });
