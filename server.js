94ab1b7b9b9a0c8d3ba20ae9d0699d2c65bde4d2
Devi Damsel <scstolp@gmail.com>
2019-03-29 12:22:04 -0600

    books

--- server.js
+++ server.js
@@ -51,10 +51,10 @@ function checkOut(req, response) {
     let query = "";
     let url = "checkOut=true";
     for (let index = 0; index < array.length; index++) {
-        console.log("value : " + array[index+1] == array.length)
-        if (array[index + 1] === array.length) {  
-            return response.status(200).redirect(`home_library.html?${url}`);
-        } else {
+        // console.log("value : " + array[index+1] == array.length)
+        // if (array[index + 1] === array.length) {  
+        //     return response.status(200).redirect(`home_library.html?${url}`);
+        // } else {
             query = `INSERT INTO patron_book (patron_id, book_title, book_id, due_date, checked_out) VALUES (1, (SELECT title FROM books WHERE book_id = $1), $2, CURRENT_DATE + interval '30' day, CURRENT_DATE)`;
             let params = [array[index], array[index]];
             pool.query(query, params, function(error, res) {
@@ -66,6 +66,10 @@ function checkOut(req, response) {
                     console.log("Item: " + array[index]);
                     url += "&array[]=" + array[index];
                     console.log("Index: " + index + " arrayLength: " + array.length);
+                         console.log("value : " + array[index+1] == array.length)
+                    if (array[index + 1] === array.length) {  
+                        return response.status(200).redirect(`home_library.html?${url}`);
+                    }
                 }
             });
         }
